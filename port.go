1cc7cc99c10cec5f9c5846edee2103be80c1360e
Stefan Majer <stefan.majer@f-i-ts.de>
2023-02-22 17:30:49 +0100

    sample

--- port.go
+++ port.go
@@ -3,6 +3,9 @@ package sonic
 import (
 	"context"
 	"fmt"
+
+	"github.com/metal-stack/sonic-go-swsssdk/api"
+	"github.com/openconfig/ygot/ygot"
 )
 
 type Port struct {
@@ -11,13 +14,29 @@ type Port struct {
 	port string
 }
 
-func (p *Port) MTU(ctx context.Context, mtu uint16) error {
+func (p *Port) MTU(ctx context.Context, port api.SonicPort_SonicPort_PORT_PORT_LIST) error {
 	existing, err := p.db.Get(ctx, p.t, AsKey(p.port))
 	if err != nil {
 		return err
 	}
 
-	existing["mtu"] = fmt.Sprintf("%d", mtu)
+	old := &api.SonicPort_SonicPort_PORT_PORT_LIST{}
+	if err := api.Unmarshal([]byte(existing), old); err != nil {
+		return fmt.Errorf("Cannot unmarshal JSON: %v", err)
+	}
 
-	return p.db.Set(ctx, p.t, AsKey(p.port), existing)
+	newPort := old
+	newPort.Mtu = port.Mtu
+
+	js, err := ygot.EmitJSON(newPort, &ygot.EmitJSONConfig{
+		Format: ygot.RFC7951,
+		RFC7951Config: &ygot.RFC7951JSONConfig{
+			AppendModuleName: true,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return p.db.Set(ctx, p.t, AsKey(p.port), js)
 }
